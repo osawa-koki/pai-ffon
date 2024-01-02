eaf702488f2f057d2bdf13eede31cbbc5afe2e07
osawa-koki <koki@osawa.dev>
2024-01-02 17:06:53 +0900

    背景の線の表示まで。

---
+++ src/elements.ts
@@ -0,0 +1,13 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+
+export const svgElement = document.getElementById('svg')!
+
+export const inCountElement = document.getElementById('in-count')!
+export const outCountElement = document.getElementById('out-count')!
+
+export const inRatioElement = document.getElementById('in-ratio')!
+export const outRatioElement = document.getElementById('out-ratio')!
+
+export const paiElement = document.getElementById('pai')!
+
+/* eslint-enable @typescript-eslint/no-non-null-assertion */
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log('Hello, world')
+import { svgElement } from './elements'
+
+// 背景の線のY座標を格納する配列。
+const lineYs = []
+
+// 背景の線を描画する。
+for (let i = 0; i <= 100; i = i + 10) {
+  const line = document.createElementNS('http://www.w3.org/2000/svg', 'polyline')
+  line.setAttribute('points', `0 ${i}, 100 ${i}`)
+  line.classList.add('background-line')
+  svgElement.appendChild(line)
+  lineYs.push(i)
+}
